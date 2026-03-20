587d5eee3d6b28122f371007c1f597961133e7f6
Allen Carvallo <crvlhallu@gmail.com>
2026-03-20 19:23:08 +0530

    Update Invoice API

--- Endpoints/InvoiceEndpoints.cs
+++ Endpoints/InvoiceEndpoints.cs
@@ -47,6 +47,33 @@ namespace CosmosApi.Endpoints
 
                 return saved > 0 ? TypedResults.Ok(true) : TypedResults.BadRequest(false);
             });
+
+            app.MapPut("/invoices/{id}", async Task<IResult> (long invoiceId, CreateInvoiceRequest req, AppDbContext db) =>
+            {
+                var invoice = await db.Invoices
+                    .Include(i => i.InvoiceItems)
+                    .FirstOrDefaultAsync(i => i.InvoiceId == invoiceId);
+
+                if (invoice is null)
+                {
+                    return TypedResults.NotFound();
+                }
+
+                invoice.InvoiceDate = req.InvoiceDate;
+                invoice.InvoiceAmount = req.InvoiceAmount;
+                invoice.CustomerId = req.CustomerId;
+                invoice.InvoiceItems = req.InvoiceItems
+                    .Select(i => new InvoiceItem
+                    {
+                        Description = i.Description,
+                        Quantity = i.Quantity,
+                        Rate = i.Rate,
+                        Amount = i.Amount,
+                    }).ToList();
+
+                var saved = await db.SaveChangesAsync();
+                return saved > 0 ? TypedResults.Ok(invoice) : TypedResults.BadRequest();
+            });
         }
     }
 }
